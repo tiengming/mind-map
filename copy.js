b6e3c7729c924f4278aee6eea37a0f55941a4c2a
tiengming <37439881+tiengming@users.noreply.github.com>
2025-06-24 09:48:52 +0800

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 
-const src = path.resolve(__dirname, './dist/index.html') 
-const dest = path.resolve(__dirname, './index.html') 
+const distIndex = path.resolve(__dirname, './dist/index.html')
+const rootIndex = path.resolve(__dirname, './index.html')
 
-if (fs.existsSync(dest)) {
-    fs.unlinkSync(dest)
+// 将 dist/index.html 拷贝到根目录
+if (fs.existsSync(distIndex)) {
+  fs.copyFileSync(distIndex, rootIndex)
 }
 
-if (fs.existsSync(src)) {
-    fs.copyFileSync(src, dest)
-    fs.unlinkSync(src)
+// 反向：确保部署产物中也包含根 index.html（静态托管入口）
+if (fs.existsSync(rootIndex)) {
+  fs.copyFileSync(rootIndex, distIndex)
 }
+
 
 // console.warn('请检查付费插件是否启用！！！')
